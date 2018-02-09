20fea231f058ea6a8d24015a035f8dca14680f21
Jiri Hajek <jh@jirihajek.net>
2018-02-09 11:06:54 +0100

    Basic CollectionListItem pop-up menu

--- webui/src/Fragments/CollectionListItem.js
+++ webui/src/Fragments/CollectionListItem.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
 import { ListItem, ListItemText } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import Menu, { MenuItem } from 'material-ui/Menu';
 
 import CollectionIcon from 'Fragments/CollectionIcon';
+import MenuIcon from 'material-ui-icons/MoreVert';
 
 const styles = {
 };
 
+// TODO: The hover effect is currently hacked by global CSS, should probably better be handled locally, but how?
+
 class CollectionListItem extends Component {
+	state = {
+		anchorEl: null,
+	}
+
+	handleMenuClick(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		this.setState({ anchorEl: event.currentTarget });
+	}
+
+	handleMenuClose() {
+		this.setState({ anchorEl: null });
+	}
+
+	handleDelete(event) {
+		event.stopPropagation();
+		event.preventDefault();
+		this.handleMenuClose();
+	}
+
 	render() {
 		return (
 			<ListItem
 				button
 				key={this.props.id}
+				className='listItem'
 				onClick={this.props.onClick}>
 				<Avatar>
 					<CollectionIcon type={this.props.type} />
@@ -22,6 +48,17 @@ class CollectionListItem extends Component {
 					primary={this.props.name}
 					secondary={this.props.folder ? 'Folder: ' + this.props.folder : ''}
 				/>
+				<IconButton aria-label='ItemMenu' className='itemButtonOnHover' onClick={this.handleMenuClick.bind(this)}>
+					<MenuIcon />
+				</IconButton>
+				<Menu
+					id="collectionmenu"
+					anchorEl={this.state.anchorEl}
+					open={Boolean(this.state.anchorEl)}
+					onClose={this.handleMenuClose.bind(this)}
+				>
+					<MenuItem onClick={this.handleDelete.bind(this)}>Delete</MenuItem>
+				</Menu>
 			</ListItem>
 		);
 	}
