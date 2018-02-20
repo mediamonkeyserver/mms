905bf9643e80b52698cec5a520dfc80fc03bfd43
Jiri Hajek <jh@jirihajek.net>
2018-02-20 16:11:00 +0100

    Formatting fix

--- webui/src/LoginIcon.js
+++ webui/src/LoginIcon.js
@@ -8,90 +8,90 @@ import Menu, { MenuItem } from 'material-ui/Menu';
 
 import Button from 'material-ui/Button';
 import Dialog, {
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
 } from 'material-ui/Dialog';
 
 const styles = {
 };
 
 class LoginIcon extends React.Component {
-  state = {
-    auth: true,
-    anchorEl: null,
-    notImpDialog: false
-  };
+	state = {
+		auth: true,
+		anchorEl: null,
+		notImpDialog: false
+	};
 
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+	handleMenu = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-  };
+	handleMenuClose = () => {
+		this.setState({ anchorEl: null });
+	};
 
-  showProfile = () => {
-    this.handleMenuClose();
-    this.setState({ notImpDialog: true });
-  }
+	showProfile = () => {
+		this.handleMenuClose();
+		this.setState({ notImpDialog: true });
+	}
 
-  handleDialogClose = () => {
-    this.setState({ notImpDialog: false });
-  }
+	handleDialogClose = () => {
+		this.setState({ notImpDialog: false });
+	}
 
-  render() {
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
+	render() {
+		const { auth, anchorEl } = this.state;
+		const open = Boolean(anchorEl);
 
-    return (
-      auth && (
-        <div>
-          <IconButton
-            aria-owns={open ? 'menu-appbar' : null}
-            aria-haspopup="true"
-            onClick={this.handleMenu}
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={open}
-            onClose={this.handleMenuClose}
-          >
-            <MenuItem onClick={this.showProfile}>Profile</MenuItem>
-            <MenuItem onClick={this.showProfile}>My account</MenuItem>
-          </Menu>
-          <Dialog
-            open={this.state.notImpDialog}
-            onClose={this.handleDialogClose}>
-            <DialogTitle>{"Not implemented"}</DialogTitle>
-            <DialogContent>
-              <DialogContentText>User and session management isn't implemented yet. It will be! ;-)</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleDialogClose} color="primary" autoFocus>Close</Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-      )
-    );
-  }
+		return (
+			auth && (
+				<div>
+					<IconButton
+						aria-owns={open ? 'menu-appbar' : null}
+						aria-haspopup="true"
+						onClick={this.handleMenu}
+						color="inherit"
+					>
+						<AccountCircle />
+					</IconButton>
+					<Menu
+						id="menu-appbar"
+						anchorEl={anchorEl}
+						anchorOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						transformOrigin={{
+							vertical: 'top',
+							horizontal: 'right',
+						}}
+						open={open}
+						onClose={this.handleMenuClose}
+					>
+						<MenuItem onClick={this.showProfile}>Profile</MenuItem>
+						<MenuItem onClick={this.showProfile}>My account</MenuItem>
+					</Menu>
+					<Dialog
+						open={this.state.notImpDialog}
+						onClose={this.handleDialogClose}>
+						<DialogTitle>{'Not implemented'}</DialogTitle>
+						<DialogContent>
+							<DialogContentText>{'User and session management isn\'t implemented yet. It will be! ;-)'}</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={this.handleDialogClose} color="primary" autoFocus>Close</Button>
+						</DialogActions>
+					</Dialog>
+				</div>
+			)
+		);
+	}
 }
 
 LoginIcon.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(LoginIcon);
