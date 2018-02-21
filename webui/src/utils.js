a8d06757da667a1cea0cb3363c90863476d1cb5d
Jiri Hajek <jh@jirihajek.net>
2018-02-21 14:43:23 +0100

    Fix formatting.

--- webui/src/utils.js
+++ webui/src/utils.js
@@ -1,25 +1,25 @@
 class Path {
-    static assertLastSeparator = (path) => {
-        if (path.endsWith('/'))
-            return path;
-        else
-            return path+'/';
-    }
+	static assertLastSeparator = (path) => {
+		if (path.endsWith('/'))
+			return path;
+		else
+			return path + '/';
+	}
 
-    static removeLastSeparator = (path) => {
-        if (path.endsWith('/'))
-            return path.substr(0, path.length-1);
-        else
-            return path;
-    }
+	static removeLastSeparator = (path) => {
+		if (path.endsWith('/'))
+			return path.substr(0, path.length - 1);
+		else
+			return path;
+	}
 
-    static join = (path1, path2) => {
-        return Path.assertLastSeparator(path1) + path2;
-    }
+	static join = (path1, path2) => {
+		return Path.assertLastSeparator(path1) + path2;
+	}
 
-    static levelUp = (path) => {
-        return path.substr(0, Path.removeLastSeparator(path).lastIndexOf('/')+1);
-    }
+	static levelUp = (path) => {
+		return path.substr(0, Path.removeLastSeparator(path).lastIndexOf('/') + 1);
+	}
 }
 
 export default Path;
