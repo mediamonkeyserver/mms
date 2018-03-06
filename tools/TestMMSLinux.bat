42254c4d9b991ebc594328703973bb65de037b40
Ludek <spiker@seznam.cz>
2018-03-06 21:53:10 +0100

    Changed default port to 10222 (uncommited change)

--- tools/TestMMSLinux.bat
+++ tools/TestMMSLinux.bat
@@ -1,1 +1,1 @@
-docker run --rm --expose=10293 -p 10222:10222/tcp -v %CD%/../dist/linux:/mms ubuntu /mms/mms
+docker run --rm --expose=10222 -p 10222:10222/tcp -v %CD%/../dist/linux:/mms ubuntu /mms/mms
