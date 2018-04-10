83568202fee8615eda8d45f141d0413f57ceb8a4
Jiri Hajek <jh@jirihajek.net>
2018-04-10 22:45:57 +0200

    Fixed build code.

--- scripts/buildUI.js
+++ scripts/buildUI.js
@@ -7,7 +7,6 @@ console.log(`Using ${npm}for processing.`); // eslint-disable-line no-console
 shell.rm('-r', 'build-webui');
 shell.cd('webui');
 shell.exec(npm + 'install');
-shell.exec(npm + 'build');
-// shell.cp('-r', 'build/', '../build-webui/');
+shell.exec(npm + 'run build');
 shell.mv('build', '../build-webui');
 shell.cd('..');
