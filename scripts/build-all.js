02ae11a7f21e4045afcd48be488b09ad45eb79ea
Jiri Hajek <jh@jirihajek.net>
2018-04-26 13:38:23 +0200

    linux 32bit build preparation

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -20,6 +20,12 @@ shell.exec(npm + 'run pkg -t node8-macos-x64 -o dist/mac64/mms .');
 shell.cp('../sqlitebinaries/node-v57-darwin-x64/*', 'dist/mac64');
 shell.cp('../ffmpegbinaries/macOS/*', 'dist/mac64');
 
+// We don't have sqlite for linux 32bit build yet.
+// shell.mkdir('-p', 'dist/linux32');
+// shell.exec(npm + 'run pkg -t node8-linux-x86 -o dist/linux32/mms .');
+// shell.cp('../sqlitebinaries/node-v57-linux-x86/*', 'dist/linux32');
+// shell.cp('../ffmpegbinaries/linux32/*', 'dist/linux32');
+
 shell.mkdir('-p', 'dist/linux64');
 shell.exec(npm + 'run pkg -t node8-linux-x64 -o dist/linux64/mms .');
 shell.cp('../sqlitebinaries/node-v57-linux-x64/*', 'dist/linux64');
