0aa51f86fd1f7545cec64cbd03418fc513046121
Jiri Hajek <jh@jirihajek.net>
2019-03-04 12:54:42 +0100

    Some testing code for QNAP compilation

--- scripts/x86-linux.docker/buildMMS.sh
+++ scripts/x86-linux.docker/buildMMS.sh
@@ -12,14 +12,18 @@ apt-get install -y nodejs
 
 # apt install gcc-3.4 g++-3.4
 
+ gcc '-DNODE_GYP_MODULE_NAME=sqlite3' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_REENTRANT=1' '-DSQLITE_THREADSAFE=1' '-DSQLITE_ENABLE_FTS3' '-DSQLITE_ENABLE_FTS4' '-DSQLITE_ENABLE_FTS5' '-DSQLITE_ENABLE_JSON1' '-DSQLITE_ENABLE_RTREE' '-DNDEBUG' -I/root/.node-gyp/8.15.0/include/node -I/root/.node-gyp/8.15.0/src -I/root/.node-gyp/8.15.0/deps/openssl/config -I/root/.node-gyp/8.15.0/deps/openssl/openssl/include -I/root/.node-gyp/8.15.0/deps/uv/include -I/root/.node-gyp/8.15.0/deps/zlib -I/root/.node-gyp/8.15.0/deps/v8/include -I./Release/obj/gen/sqlite-autoconf-3260000  -std=c99 -pthread -Wall -Wextra -Wno-unused-parameter -m32 -O3 -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/sqlite3/gen/sqlite-autoconf-3260000/sqlite3.o.d.raw -c -o Release/obj.target/sqlite3/gen/sqlite-autoconf-3260000/sqlite3.o Release/obj/gen/sqlite-autoconf-3260000/sqlite3.c
+
+gcc '-DNODE_GYP_MODULE_NAME=sqlite3' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_REENTRANT=1' '-DSQLITE_THREADSAFE=1' '-DSQLITE_ENABLE_FTS3' '-DSQLITE_ENABLE_FTS4' '-DSQLITE_ENABLE_FTS5' '-DSQLITE_ENABLE_JSON1' '-DSQLITE_ENABLE_RTREE' '-DNDEBUG' -std=c99 -pthread -Wall -Wextra -Wno-unused-parameter -m32 -O3 -fno-omit-frame-pointer  -MMD -MF ./sqlite3.o.d.raw -c -o sqlite3.o sqlite3.c
+
 export CC="/CT/i686-QNAP-linux-gnu/cross-tools/bin/i686-QNAP-linux-gnu-gcc"
 export CXX="/CT/i686-QNAP-linux-gnu/cross-tools/bin/i686-QNAP-linux-gnu-g++"
 export LINK="/CT/i686-QNAP-linux-gnu/cross-tools/bin/i686-QNAP-linux-gnu-g++"
 export SOLINK="/CT/i686-QNAP-linux-gnu/cross-tools/bin/i686-QNAP-linux-gnu-g++"
 
 
-export CC="/opt/cross-project/x86/sys-root/usr/bin/gcc"
-export CXX="/opt/cross-project/x86/sys-root/usr/bin/g++"
+export CC="/opt/cross-project/x86/sys-root/usr/bin/gcc -I/opt/cross-project/x86/sys-root/usr/lib/gcc/i486-linux-gnu/4.1.2/include/"
+export CXX="/opt/cross-project/x86/sys-root/usr/bin/g++ -I/opt/cross-project/x86/sys-root/usr/lib/gcc/i486-linux-gnu/4.1.2/include/"
 export LINK="/opt/cross-project/x86/sys-root/usr/bin/g++"
 export SOLINK="/opt/cross-project/x86/sys-root/usr/bin/g++"
 
@@ -44,7 +48,11 @@ export CC=gcc-4.8
 
 export CXX=g++
 export CC=gcc
-npm install sqlite3 --build-from-source --verbose
+
+cd /
+mkdir sqlite
+cd sqlite
+npm install sqlite3 --build-from-source --target_arch=ia32
 
 cd /
 git clone https://github.com/mediamonkeyserver/mms.git
