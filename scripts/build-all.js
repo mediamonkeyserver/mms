a8431937e86a13b747896527cac8018f31a455ea
Jiri Hajek <jh@jirihajek.net>
2019-03-21 16:18:26 +0100

    Fixed packaged Windows tray icon functionality

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -10,14 +10,27 @@ console.log(`Using ${npm}for building all platforms.`); // eslint-disable-line n
 
 const version = JSON.parse(fs.readFileSync('package.json')).version;
 
-shell.mkdir('-p', 'dist/win64');
+// *** Win 64 bit ***
+shell.mkdir('-p', 'dist/win64/icon');
 shell.exec(npm + 'run pkg -t node8-win-x64 -o dist/win64/mms.exe .');
 shell.cp('../sqlitebinaries/node-v57-win32-x64/*', 'dist/win64'); // Currently stored out of this git repository, change it later.
 shell.cp('../ffmpegbinaries/win64/*', 'dist/win64'); // Currently stored out of this git repository, change it later.
+shell.cp('icon/mm.ico', 'dist/win64/icon');
+
+// So that mms.exe works with native tray icon module
+// More info at https://www.ironsrc.com/blog/rename-import-dll/ and https://github.com/parro-it/libui-node/issues/120#issuecomment-425627593
+const bin = 'node_modules\\windows-trayicon\\build\\Release\\nbind.node';
+const binBack = bin+'.back';
+shell.cp(bin, binBack);
+shell.exec(`scripts\\rid.exe ${bin} node.exe mms.exe`);
+shell.cp(bin, 'dist/win64');
+shell.cp(binBack, bin);
+
 shell.cd('dist/win64');
 shell.exec(`${winrar} a ../MMS-win64-${version}.rar * -s -md64 -ma`);
 shell.cd('../..');
 
+// *** Win 32 bit ***
 shell.mkdir('-p', 'dist/win32');
 shell.exec(npm + 'run pkg -t node8-win-x86 -o dist/win32/mms.exe .');
 shell.cp('../sqlitebinaries/node-v57-win32-ia32/*', 'dist/win32');
@@ -26,6 +39,7 @@ shell.cd('dist/win32');
 shell.exec(`${winrar} a ../MMS-win32-${version}.rar * -s -md64 -ma`);
 shell.cd('../..');
 
+// *** Mac ***
 shell.mkdir('-p', 'dist/mac64');
 shell.exec(npm + 'run pkg -t node8-macos-x64 -o dist/mac64/mms .');
 shell.cp('../sqlitebinaries/node-v57-darwin-x64/*', 'dist/mac64');
@@ -34,6 +48,7 @@ shell.cd('dist/mac64');
 shell.exec(`${winrar} a ../MMS-mac64-${version}.rar * -s -md64 -ma`);
 shell.cd('../..');
 
+// *** Linux ***
 shell.mkdir('-p', 'dist/linux64');
 shell.exec(npm + 'run pkg -t node8-linux-x64 -o dist/linux64/mms .');
 shell.cp('../sqlitebinaries/node-v57-linux-x64/*', 'dist/linux64');
@@ -41,6 +56,7 @@ shell.cp('../ffmpegbinaries/linux64/*', 'dist/linux64');
 shell.exec(`docker run --rm -v %CD%/dist/:/dist ubuntu /bin/bash -c "cd /dist/linux64; tar cfz ../MMS-linux64-${version}.tar.gz *"`);
 shell.exec(`docker run --rm -v %CD%/dist/:/dist ubuntu /bin/bash -c "apt-get update; apt-get install xz-utils; cd /dist/linux64; tar cfJ ../MMS-linux64-${version}.tar.xz *"`);
 
+// *** QNAP x86 ***
 const nodelinux64 = '~/.pkg-cache/v2.5/fetched-v8.11.3-linux-x64';
 shell.mv(nodelinux64, nodelinux64+'.back');
 shell.cp('../nodebinaries/node-linux64-QNAPx86', nodelinux64);
--- scripts/packWin.js
+++ scripts/packWin.js
@@ -5,5 +5,15 @@ const shell = require('shelljs');
 
 const npm = (shell.which('yarn') ? 'yarn ' : 'npm ');
 
-shell.mkdir('-p', 'dist/win64');
+shell.mkdir('-p', 'dist/win64/icon');
 shell.exec(npm + 'run pkg -t node8-win-x64 -o dist/win64/mms.exe .');
+shell.cp('icon/mm.ico', 'dist/win64/icon');
+
+// So that mms.exe works with native tray icon module
+// More info at https://www.ironsrc.com/blog/rename-import-dll/ and https://github.com/parro-it/libui-node/issues/120#issuecomment-425627593
+const bin = 'node_modules\\windows-trayicon\\build\\Release\\nbind.node';
+const binBack = bin+'.back';
+shell.cp(bin, binBack);
+shell.exec(`scripts\\rid.exe ${bin} node.exe mms.exe`);
+shell.cp(bin, 'dist/win64');
+shell.cp(binBack, bin);
