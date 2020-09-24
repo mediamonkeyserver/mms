750c75e4a89126b7b105906dd67342e6f60e6d8e
Jiri Hajek <jh@jirihajek.net>
2020-09-24 15:27:17 +0200

    Build script more robust

--- scripts/build-arm.js
+++ scripts/build-arm.js
@@ -9,4 +9,4 @@ shell.mkdir('-p', 'dist/linux-armv7');
 // shell.exec(npm + 'run pkg -t node8-linux-armv7 -o dist/linux-armv7/mms .'); // JH: Not sure why this doesn't work on my Raspberry?
 shell.exec('node node_modules/.bin/pkg -t node8-linux-armv7 -o dist/linux-armv7/mms .');
 shell.cp('node_modules/@mmserver/sqlite3/lib/binding/node-v57-linux-arm/*', 'dist/linux-armv7');
-shell.cp('../ffmpegbinaries/linux-armv7/*', 'dist/linux-armv7');
+shell.cp('binaries/ffmpeg/linux-armv7/*', 'dist/linux-armv7');
