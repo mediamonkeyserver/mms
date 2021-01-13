18b6e3df27befa3320b75573123d5ba00f971cc4
Jiri Hajek <jh@jirihajek.net>
2021-01-13 13:46:13 +0100

    Fixes in armv8 build (not successful yet)

--- scripts/arm64v8.docker/buildMMS.sh
+++ scripts/arm64v8.docker/buildMMS.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 cd ~
 . ~/.nvm/nvm.sh   # to make nvm usable from shell
-nvm install 8.15.0
-nvm use 8.15.0
+nvm install 10.21.0
+nvm use 10.21.0
 
 cd ~
-mkdir -p .pkg-cache/v2.5
-cd ~/.pkg-cache/v2.5
+mkdir -p .pkg-cache/v2.6
+cd ~/.pkg-cache/v2.6
 cp /buildTarget/node-arm64 ./fetched-v8.11.3-linux-arm64
-#wget https://github.com/robertsLando/pkg-binaries/raw/master/arm64/fetched-v8.11.3-linux-arm64
 
 cd /
 git clone https://github.com/mediamonkeyserver/mms.git
