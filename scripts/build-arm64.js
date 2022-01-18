9c35f5b486a8f00689ba0fe69dbc7c3b878c7e05
Jiri Hajek <jh@jirihajek.net>
2022-01-18 12:08:09 +0100

    Build script fix

--- scripts/build-arm64.js
+++ scripts/build-arm64.js
@@ -9,7 +9,7 @@ const version = JSON.parse(fs.readFileSync('package.json')).version;
 shell.mkdir('-p', 'dist/linux-arm64');
 shell.exec('node_modules/pkg/lib-es5/bin.js -t node16-linux-arm64 -o dist/linux-arm64/mms .');
 shell.cp('node_modules/better-sqlite3/build/Release/better_sqlite3.node', 'dist/linux-arm64');
-shell.cp('../binaries/ffmpeg/linux-arm64/*', 'dist/linux-arm64');
+shell.cp('binaries/ffmpeg/linux-arm64/*', 'dist/linux-arm64');
 shell.cd('dist/linux-arm64');
 shell.exec(`tar cfz ../MMS-arm64-${version}.tar.gz *`);
 shell.cd('../..');
